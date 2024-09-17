42b35a9e0d5706749fa4400db7ba26245bcfd482
Juliana Zupo <123331586+Juzupo@users.noreply.github.com>
2024-09-17 15:44:56 +0100

    melhoria do estilo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import ProductsList from './components/ProductsList.jsx';
 import Cart from './components/Cart.jsx';
+import './index.css';
 
 const App = () => {
   const [cart, setCart] = useState([]);
@@ -16,13 +17,32 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Mini Loja Virtual</h1>
+      {/* Cabeçalho com Logo e Menu */}
+      <header>
+        <nav>
+          <div className="logo">Mini Loja</div>
+          <ul className="menu">
+            <li>Home</li>
+            <li>Produtos</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      </header>
+
+      <h1>Bem-vindo à Mini Loja Virtual!</h1>
+
       <div className="main-content">
         <ProductsList addToCart={addToCart} />
         <Cart cart={cart} removeFromCart={removeFromCart} />
       </div>
+
+      {/* Rodapé */}
+      <footer>
+        <p>Mini Loja Virtual © 2024 - Todos os Direitos Reservados</p>
+      </footer>
     </div>
   );
 };
 
 export default App;
+
