da336d40c6761fd3bd9f470c2f9e99ba63f5a831
Gabriel-Magalhaes <jorgemagalhaes22.15@gmail.com>
2024-09-20 18:19:10 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,49 @@ const App = () => {
         <Cart cart={cart} removeFromCart={removeFromCart} />
       </div>
 
+      <div className='grid-Content'>
+        <div className="grid-product">
+            <img src="" alt="imagem_produto" className='img-product' />
+            <p className='preco'>19,99</p>
+            <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorem, ipsam veritatis assumenda harum repellendus provident consequatur odio, libero, temporibus rem.</p>
+            <button className='add-product'>Adicionar ao Carrinho</button>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+        <div className="grid-product">
+          <h2>Produtos</h2>
+        </div>
+
+      </div>
+
       {/* Rodapé */}
       <footer>
         <p>Mini Loja Virtual © 2024 - Todos os Direitos Reservados</p>
